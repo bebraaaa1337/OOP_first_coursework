46663dac9758fafb46d65ee4b3462313a427ed17
bebraaaa1337 <artur.filatov3010200@gmail.com>
2025-04-23 11:54:16 +0300

    add normilize

--- ML_Library/linear_module.py
+++ ML_Library/linear_module.py
@@ -1,14 +1,27 @@
+
 from random import randint
 class Linear_Regression:
-    def __init__(self, weight_size=1,bias=False):
+    def __init__(self, weight_size=1,bias=False, learning_rate=0.01):
         self.weight_size=weight_size
         self.bias=bias
+        self.learning_rate=0.01
         self.w=[1]*(weight_size+bias)
-        for i in range(weight_size):
-            self.w[i]=randint(-10,10)
+        for i in range(weight_size+bias):
+            self.w[i]=randint(-10,10)*0.01
+        self.mean=None
+        self.std=None
+    def normilize(self,x):
+        self.mean=[0]*self.weight.size()
+        for i in range (self.weight.size()):
+            for j in range (x[i]):
+                self.mean[i]+=x[i][j]
+            self.mean[i]=self.mean[i]/len(x[i])
+        self.std=[0]*self.weight_size
+
     def predict(self,x):
         if len(x)!=self.weight_size:
             print('PROBLEM')
+        x_
         ans=0
         for i in range(self.weight_size):
             ans+=x[i]*self.w[i]
--- ML_Library/losses_module.py
+++ ML_Library/losses_module.py
@@ -3,8 +3,6 @@ class MSE_Loss:
         self.grad=0
         self.loss=0
     def get_loss(self,y_pred,y_true):
-        self.grad=0
-        self.loss=0
         self.loss=(y_pred-y_true)**2
         self.grad=2*(y_pred-y_true)
         return self.loss
